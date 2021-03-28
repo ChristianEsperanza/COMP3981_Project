bf4b1e39c22d5700364189ff466eba2d4d0d09a5
ChristianEsperanza <53631340+ChristianEsperanza@users.noreply.github.com>
2021-03-27 19:58:53 -0700

    Template for controls

---
+++ GUI/gui_controls.py
@@ -0,0 +1,19 @@
+# Callback functions
+import GUI
+
+
+def start_game_button(context: GUI):
+    print("starting game")
+
+def stop_game_button(context: GUI):
+    print("Stopping game")
+
+def pause_game_button(context: GUI):
+    print("Pausing game")
+
+def reset_game_button(context: GUI):
+    print("Resetting game")
+
+def undo_move_button(context: GUI):
+    print("Undoing move")
+
