20752561f885ee65a65081f55a0bd5dc4ff98a87
ChristianEsperanza <53631340+ChristianEsperanza@users.noreply.github.com>
2021-04-11 17:38:41 -0700

    Added test board, removed unnecessary strip_coordinate calls in ai_main, added TODOs for testing, move log shows up on start, primed for movement changes

--- AI/ai_main.py
+++ AI/ai_main.py
@@ -12,10 +12,10 @@ from Utility.constants import *
 def begin_turn(context: GUI, piece_id):
     execute_thread(context, piece_id)
 
-def execute_thread(context:GUI, piece_id):
+def execute_thread(context: GUI, piece_id):
     _thread.start_new_thread(calculate, (context, piece_id))
 
-def calculate(context:GUI, piece_id):
+def calculate(context: GUI, piece_id):
     board_state = context.board.to_string_state()
     turn = ('b', 'w')[piece_id == white_piece_id]
     best_move = Evaluator.minimax(board_state, turn)
@@ -33,29 +33,34 @@ def find_and_execute_move(best_move, context: GUI):
     and the colour.
     Ex - ['C3b','C4w']
     """
-    start_coordinates = best_move['start']
-    end_coordinates = best_move['end']
+    start_coordinates = [strip_coordinate(coordinate) for coordinate in best_move['start']]
+    end_coordinates = [strip_coordinate(coordinate) for coordinate in best_move['end']]
     pushes = best_move['pushes']
-    # Start/end coordinates are a string if 1 move, list otherwise
+
 
     # Pushing and sumito
     if pushes > 0:
+        enemy_end = best_move['e_end']
+
         # Two marbles pushing
         if len(start_coordinates) == 2:
+
+            # TODO: Test this
             # Sumito two to one
             if best_move['elim']:
-                movement.sumito_two_to_one(context, strip_coordinate(start_coordinates[0]), strip_coordinate(start_coordinates[1]),
-                                                            strip_coordinate(end_coordinates[0]), strip_coordinate(end_coordinates[1]))
+                movement.sumito_two_to_one(context, start_coordinates[0], start_coordinates[1],
+                                           end_coordinates[0], end_coordinates[1])
                 game_state.add_to_move_history(context, start_coordinates, end_coordinates)
 
+            # TODO: Test this
             # Push two to one
             else:
                 # IE D5D6 - D6D7w
                 vector = best_move['move'].value
                 target_coordinate = get_next_coordinate(end_coordinates[1], vector)
 
-                movement.push_two_to_one(context, strip_coordinate(start_coordinates[0]), strip_coordinate(start_coordinates[1]),
-                                        strip_coordinate(end_coordinates[0]), strip_coordinate(end_coordinates[1]), target_coordinate)
+                movement.push_two_to_one(context, start_coordinates[0], start_coordinates[1],
+                                         end_coordinates[0], end_coordinates[1], target_coordinate)
                 game_state.add_to_move_history(context, start_coordinates, end_coordinates)
 
         # 3-1 Push    (C3C4C5 -> C4C5C6)
@@ -68,66 +73,70 @@ def find_and_execute_move(best_move, context: GUI):
             if best_move['elim']:
                 vector = best_move['move'].value
 
+                # TODO: Test this
                 # Sumito 3-1
-                # Should be fine as is
                 if pushes == 1:
-                    movement.sumito_three_to_one(context, strip_coordinate(start_coordinates[0]), strip_coordinate(end_coordinates[2]))
+                    movement.sumito_three_to_one(context, start_coordinates[0], end_coordinates[2])
                     game_state.add_to_move_history(context, start_coordinates, end_coordinates)
 
+                # TODO: Test this
                 # Sumito 3-2
                 else:
 
                     # (D3D4D5) -> (D2D3D4) Pushed off d1
                     pushed_coordinate = get_next_coordinate(strip_coordinate(end_coordinates[2]), vector)
-                    movement.sumito_three_to_two(context, strip_coordinate(start_coordinates[0]), strip_coordinate(start_coordinates[1]), strip_coordinate(start_coordinates[2]),
-                                                 strip_coordinate(end_coordinates[0]), strip_coordinate(end_coordinates[1]), strip_coordinate(end_coordinates[2]),
-                                                 pushed_coordinate)
+                    movement.sumito_three_to_two(context, start_coordinates[0], start_coordinates[1],
+                                                 start_coordinates[2], end_coordinates[0], end_coordinates[1],
+                                                 end_coordinates[2], pushed_coordinate)
                     game_state.add_to_move_history(context, start_coordinates, end_coordinates)
 
             else:
-
                 vector_tuple = best_move['move'].value
 
+                # TODO: Test this
                 # Push 3-1
                 if pushes == 1:
-                    pushed_coordinate = get_next_coordinate(strip_coordinate(end_coordinates[2]), vector_tuple)
-                    movement.push_three_to_one(context, strip_coordinate(start_coordinates[0]), strip_coordinate(start_coordinates[1]), strip_coordinate(start_coordinates[2]),
-                                               strip_coordinate(end_coordinates[0]), strip_coordinate(end_coordinates[1]),
-                                               strip_coordinate(end_coordinates[2]), pushed_coordinate)
+                    pushed_coordinate = get_next_coordinate(end_coordinates[2], vector_tuple)
+                    movement.push_three_to_one(context, start_coordinates[0], start_coordinates[1],
+                                               start_coordinates[2], end_coordinates[0], end_coordinates[1],
+                                               end_coordinates[2], pushed_coordinate)
                     game_state.add_to_move_history(context, start_coordinates, end_coordinates)
 
+                # TODO: Test this
                 # Push 3-2
                 else:
                     # TODO: Pushing left/right doesn't work correctly every time
                     # Ex - (G6G7G8 -> G5G6G7) Pushed (G5G4 -> G4G3)
-                    pushed_coordinate_1 = get_next_coordinate(strip_coordinate(end_coordinates[2]), vector_tuple)
+                    pushed_coordinate_1 = get_next_coordinate(end_coordinates[2], vector_tuple)
                     pushed_coordinate_2 = get_next_coordinate(pushed_coordinate_1, vector_tuple)
 
-                    movement.push_three_to_two(context,
-                                               strip_coordinate(start_coordinates[0]), strip_coordinate(start_coordinates[1]),strip_coordinate(start_coordinates[2]),
-                                               strip_coordinate(end_coordinates[0]), strip_coordinate(end_coordinates[1]), strip_coordinate(end_coordinates[2]),
-                                               pushed_coordinate_1, pushed_coordinate_2)
+                    movement.push_three_to_two(context, start_coordinates[0], start_coordinates[1],
+                                               start_coordinates[2], end_coordinates[0], end_coordinates[1],
+                                               end_coordinates[2], pushed_coordinate_1, pushed_coordinate_2)
                     game_state.add_to_move_history(context, start_coordinates, end_coordinates)
 
     # If not pushing, move to designated coordinates
     else:
         # Move one stone
         # Start/end coordinates come in strings instead of list in this situation
-        if isinstance(start_coordinates, str):
-            movement.move_1_piece(context, strip_coordinate(start_coordinates), strip_coordinate(end_coordinates))
-            game_state.add_to_move_history(context, [start_coordinates], [end_coordinates])
+        if len(start_coordinates) == 1:
+
+            # TODO: Double check end coordinates will length of 1
+            movement.move_1_piece(context, start_coordinates[0], end_coordinates[0])
+            game_state.add_to_move_history(context, start_coordinates, end_coordinates)
 
         # Move two stones
         elif len(start_coordinates) == 2:
-            movement.move_2_pieces(context, strip_coordinate(start_coordinates[0]), strip_coordinate(start_coordinates[1]),
-                                   strip_coordinate(end_coordinates[0]), strip_coordinate(end_coordinates[1]))
+            movement.move_2_pieces(context, start_coordinates[0], start_coordinates[1], end_coordinates[0],
+                                   end_coordinates[1])
             game_state.add_to_move_history(context, start_coordinates, end_coordinates)
 
         # Move three stones
         elif len(start_coordinates) == 3:
-            movement.move_3_pieces(context, strip_coordinate(start_coordinates[0]), strip_coordinate(start_coordinates[1]), strip_coordinate(start_coordinates[2]),
-                                   strip_coordinate(end_coordinates[0]), strip_coordinate(end_coordinates[1]), strip_coordinate(end_coordinates[2]))
+            movement.move_3_pieces(context, start_coordinates[0], start_coordinates[1], start_coordinates[2],
+                                   end_coordinates[0], end_coordinates[1], end_coordinates[2])
             game_state.add_to_move_history(context, start_coordinates, end_coordinates)
+
 
 def get_next_coordinate(last_coordinate: str, vector: tuple):
     return f"{chr(ord(last_coordinate[0]) + vector[0])}{int(last_coordinate[1]) + vector[1]}"
@@ -135,4 +144,3 @@ def get_next_coordinate(last_coordinate: str, vector: tuple):
 def strip_coordinate(move_string):
     # Strip the colour from a given coordinate
     return move_string[0] + move_string[1]
-
